88d3d3b15aa32129dfc785a79ee9136b25afc768
robna <robin.lenz@io-warnemuende.de>
2021-12-03 02:24:23 +0100

    started implementing map in glm app

--- analysis/app_glm.py
+++ analysis/app_glm.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 import altair as alt
+import pydeck as pdk
 
 # from streamlit_vega_lite import altair_component
 # alt.renderers.enable('altair_viewer')  # use to display altair charts externally in browser instead of inline (only activate in non-vega-compatible IDE like pycharm)
@@ -53,6 +54,30 @@ def pdd2sdd(mp_pdd, regions):
     mp_added_sed_sdd = mp_added_sed_sdd.loc[mp_added_sed_sdd.regio_sep.isin(regions)]  # filter based on selected regions
 
     return mp_added_sed_sdd
+
+
+def map(data):
+    st.write(pdk.Deck(
+        map_style="mapbox://styles/mapbox/light-v9",
+        initial_view_state={
+            "latitude": lat,
+            "longitude": lon,
+            "zoom": zoom,
+            "pitch": 50,
+        },
+        layers=[
+            pdk.Layer(
+                "HexagonLayer",
+                data=data,
+                get_position=["lon", "lat"],
+                radius=100,
+                elevation_scale=4,
+                elevation_range=[0, 1000],
+                pickable=True,
+                extruded=True,
+            ),
+        ]
+    ))
 
 
 def poly_comp_chart(mp_pdd):
