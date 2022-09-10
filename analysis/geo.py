53c96fa31e4e916ce11cd49e95be15bc4c30cd99
roble <lenz@ipfdd.de>
2022-09-11 01:46:36 +0200

    new  feature for prediction model: WWTP_influence generated from BAW particle simulation

--- analysis/geo.py
+++ analysis/geo.py
@@ -1,4 +1,6 @@
 import geopandas as gpd
+import pandas as pd
+from shapely.geometry import LineString
 
 
 
@@ -26,3 +28,72 @@ def get_distance_to_shore(LON, LAT, polygon=None, epsg=32632):
     d = gdf.geometry.apply(lambda x: polygon.boundary.distance(x))
 
     return d
+
+
+def get_BAW_traces(epsg=25832):
+    df = pd.read_fwf('../data/insel_Bahnen.dat', names=['simPartID', 'X', 'Y', 'depth'])
+    df.loc[df.simPartID == 'Part', 'simPartID'] = df.loc[df.simPartID == 'Part', 'X']
+    df.simPartID.fillna(method='ffill', inplace=True)
+    df.dropna(inplace=True)
+    # add a new column to df called 'time_step' which starts from one for each simPartID and increases by one for each row
+    df['time_step'] = df.groupby('simPartID').cumcount() + 1
+
+    gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.X, df.Y), crs=f"EPSG:{epsg}").drop(columns=['X', 'Y'])
+    return gdf
+
+
+def cummulated_buffer_residents(sdd, gdf=None, buffer_radius=222, col_name='WWTP_influence'):
+    """
+    Calculates the sum of simulated particles that were encountered in a sample's buffer zone (irrespective of the time step).
+    :param sdd: df with sample domain data
+    :param gdf: GeoDataFrame containing the particle tracks as points gemotries per time step
+    :return: sdd df with cummulated residence time in the buffer zone
+    """
+    sdd = sdd.copy()
+    if gdf is None:
+        gdf = get_BAW_traces()
+    # turn sdd into a GeoDataFrame with conversion from Lat/Lon to to epsg of gdf
+    sdd_gdf = gpd.GeoDataFrame(sdd, geometry=gpd.points_from_xy(sdd.LON, sdd.LAT), crs=4326).to_crs(gdf.crs)
+    sdd_gdf['geometry'] = sdd_gdf.buffer(buffer_radius)
+    # spatially join the sample domain data with the particle tracks, where the buffer zone of a sample contains a particle location at any time step
+    dfsjoin = gpd.sjoin(sdd_gdf, gdf, how='left', predicate='contains')
+    # group sdd by Sample and sum the number of particles that were encountered in the buffer zone
+    sdd[col_name] = dfsjoin.reset_index().groupby('index').index_right.count()
+    return sdd
+
+##TODO: the following two functions are not woring properly yet
+def build_paths(start, polygon, lines=None):
+    """
+    Generates a line between start and each polygon vertex if it does not intersect with the polygon.
+    :param start: start point
+    :param polygon: polygon action as a boundary
+    :return: list of lines between start and each polygon vertex
+    """
+    if lines is None:
+        lines = []
+    for vertex in polygon.exterior.coords:
+        line = LineString([start, vertex])
+        if not line.intersects(polygon):
+            lines.append(line)
+    return lines
+
+
+def dist_within(start, dest, polygon):
+    """
+    Calculates the distance between two points within a polygon using the Shapely library.
+    :param start: start point
+    :param dest: destination point
+    :param polygon: polygon action as a boundary
+    :return: distance between start and dest within polygon
+    """
+    # check if start and dest are within the polygon, and raise an error if not
+    if not polygon.contains(start) or not polygon.contains(dest):
+        raise ValueError('start and dest must be within the polygon')
+    # create a line between start and dest
+    line = LineString([start, dest])
+    # check if line lies completely within the polygon, and if so: return its length
+    if polygon.contains(line):
+        return line.length
+    # generate a line between start and each polygon vertex if it does not intersect with the polygon
+    lines = build_paths(start, dest, polygon)
+    # while lines have not reached dest, extend them to the next polygon vertex
