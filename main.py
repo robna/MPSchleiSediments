b7c4d554f4585c05b7b7da844325a28253a60b82
roble <lenz@ipfdd.de>
2025-04-16 23:36:24 +0200

    refactor main.py: simplify path handling and remove unnecessary comments

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
-# Note: This file is just needed for streamlit app to run when deployed Streamlit app platform.
-#       Not needed for local run.
-#       Adapted from: https://github.com/IndigoWizard/Shinosuke-lab/tree/main
-
-import os
 import sys
+from pathlib import Path
 
-# get the path of your current script (main.py in here)
-script_path = os.path.dirname(os.path.abspath(__file__))
+SCRIPT = Path(__file__).resolve().parent
+APP   = SCRIPT / "analysis"
+sys.path.append(str(APP))
 
-# add 'analysis' folder to sys.path
-app_folder = os.path.join(script_path, 'analysis')
-sys.path.append(app_folder)
-
-# import and run the Streamlit app from app.py that lives in analysis folder
-import app
-
-if __name__ == '__main__':
-    os.chdir(app_folder)  # change the working directory to the app folder
-    app.main()  # run the Streamlit app
+if __name__ == "__main__":
+    # Stay in project root so data paths resolve correctly
+    import app
+    app.main()
